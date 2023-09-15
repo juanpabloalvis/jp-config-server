8707bc55d15c1999aa6ee82d6f014600d4f92c5a
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-15 11:22:28 -0500

    Initial commit, add repository url, change start port

---
+++ src/main/java/com/jp/config/JpConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.jp.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpConfigServerApplication.class, args);
+	}
+
+}
